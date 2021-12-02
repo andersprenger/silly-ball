f81b4a455507cb9b6037ff4d79fd82deb4e24dec
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-12-02 17:25:05 -0300

    add physics and stuff

--- silly-ball/GameScene.swift
+++ silly-ball/GameScene.swift
@@ -9,15 +9,16 @@ import SpriteKit
 import GameplayKit
 import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // imput helper vars
     var motionManager = CMMotionManager()
     var updateRate: Double = 1/60
     var yaw: Double = 0
+    private var floorCategoryBitMask: UInt32 = 1 << 0
+
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //private var node : SKShapeNode?
     
     func startControl() {
         if motionManager.isDeviceMotionAvailable{
@@ -31,8 +32,51 @@ class GameScene: SKScene {
     }
     
     override func didMove(to view: SKView) {
-
+        self.physicsWorld.contactDelegate = self
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 6.8)
+        
+        setBoundariesPhysics()
+        
+        var node1: SKShapeNode = SKShapeNode(circleOfRadius: 20)
+        node1.name = "node"
+        node1.zPosition = 0
+        node1.fillColor = .red
+        node1.position = CGPoint(x: ((self.scene?.size.width)!) / 2, y: ((self.scene?.size.height)!) / 2)
+        node1.physicsBody = SKPhysicsBody(circleOfRadius: 20)
+        // node1.physicsBody?.isDynamic = false
+        addChild(node1)
     }
+    
+    func setBoundariesPhysics() {
+            
+            // Floor creation
+            let sky = SKSpriteNode(color: .red, size: CGSize(width: (self.scene?.size.width)!, height: 1.0))
+            sky.name = "Floor"
+            sky.position = CGPoint(x: ((self.scene?.size.width)!) / 2, y: (self.scene?.size.height)!)
+            sky.zPosition = 0
+            
+            // Floor Physics
+            let floorPhysicsBody = SKPhysicsBody(rectangleOf: sky.frame.size)
+            floorPhysicsBody.isDynamic = false
+            floorPhysicsBody.categoryBitMask = floorCategoryBitMask
+            floorPhysicsBody.contactTestBitMask = 00000100
+            sky.physicsBody = floorPhysicsBody
+            
+            
+            // Sky creation
+            let floor = SKSpriteNode(color: .red, size: CGSize(width: (self.scene?.size.width)!, height: 10.0))
+            floor.name = "Sky"
+            floor.position = CGPoint(x: ((self.scene?.size.width)!) / 2, y: 0)
+            floor.zPosition = 0
+            
+            // Sky Physics
+            let skyPhysicsBody = SKPhysicsBody(rectangleOf: floor.frame.size)
+            skyPhysicsBody.isDynamic = false
+            floor.physicsBody = skyPhysicsBody
+            
+            addChild(sky)
+            addChild(floor)
+        }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
